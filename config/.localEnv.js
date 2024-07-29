c6b8d5daefd3280c7d2e5d48dcad59df1a37e8fe
maham-a-awais <mahamawais888@outlook.com>
2024-07-29 17:39:58 +0500

    structured api-response,forgotpassword, error function

--- config/.localEnv.js
+++ config/.localEnv.js
@@ -5,7 +5,8 @@ module.exports = {
   HOST: process.env.DB_HOST,
   DATABASE: process.env.DB_NAME,
   EMAIL: process.env.MY_EMAIL,
-  PASSWORD: process.env.MY_PASSWORD,
+  EMAIL_PASSWORD: process.env.MY_PASSWORD,
   SECRET_KEY: process.env.SECRET_KEY,
   JWT_EXPIRATION: process.env.JWT_EXPIRATION,
+  PASSWORD_RESET_EXPIRATION: process.env.PASSWORD_RESET_EXPIRATION,
 };
