ac487eb244df95075b008e9cd10953281aa582c4
maham-a-awais <mahamawais888@outlook.com>
2024-08-13 12:56:18 +0500

    pool

--- config/database.js
+++ config/database.js
@@ -2,19 +2,23 @@ const { HOST, PASSWORD, USERNAME, DATABASE, DIALECT } = require(".");
 const sequelize = require("sequelize");
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-});
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL,
+// });
 
-const db = new sequelize(DATABASE, USERNAME, PASSWORD, {
-  host: HOST,
-  dialect: DIALECT,
-  pool: pool,
-});
+// const db = new sequelize(DATABASE, USERNAME, PASSWORD, {
+//   host: HOST,
+//   dialect: DIALECT,
+//   pool: pool,
+// });
 
-pool.connect((err) => {
-  if (err) throw err;
-  console.log("Connect to PostgreSQL successfully!");
+// pool.connect((err) => {
+//   if (err) throw err;
+//   console.log("Connect to PostgreSQL successfully!");
+// });
+
+const db = new sequelize(process.env.POSTGRES_URL, {
+  dialect: "postgres",
 });
 module.exports = db;
 
