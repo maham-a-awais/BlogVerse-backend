2c0c91f891af370568beb23e1c53e5d1364bbb83
maham-a-awais <mahamawais888@outlook.com>
2024-08-13 11:11:24 +0500

    vercel sequelize

--- config/database.js
+++ config/database.js
@@ -1,11 +1,23 @@
 const { HOST, PASSWORD, USERNAME, DATABASE, DIALECT } = require(".");
 const sequelize = require("sequelize");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
+
 const db = new sequelize(DATABASE, USERNAME, PASSWORD, {
   host: HOST,
   dialect: DIALECT,
+  pool: pool,
 });
 
 module.exports = db;
+
+pool.connect((err) => {
+  if (err) throw err;
+  console.log("Connect to PostgreSQL successfully!");
+});
 
 //const config = require("./config")
 //const sequelize = new sequelize(config[env]);
