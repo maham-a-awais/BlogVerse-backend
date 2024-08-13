1a5d1139eb21b8f879c7fff8d3395a6526fa1736
maham-a-awais <mahamawais888@outlook.com>
2024-08-13 12:49:59 +0500

    db

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const sequelize = require("sequelize");
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  connectionString: process.env.POSTGRES_URL,
 });
 
 const db = new sequelize(DATABASE, USERNAME, PASSWORD, {
