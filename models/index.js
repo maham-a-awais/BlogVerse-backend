088adc9d284cb310729b0c0ca324433f8f70bff7
maham-a-awais <mahamawais888@outlook.com>
2024-08-13 14:20:26 +0500

    new

--- models/index.js
+++ models/index.js
@@ -9,6 +9,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
 const con = require("../config/index");
 const db = {};
+const pg = require("pg");
 
 let sequelize;
 if (config.use_env_variable) {
