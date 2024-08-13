ee6720a508568d80e38251d30d9b2693faea8270
maham-a-awais <mahamawais888@outlook.com>
2024-08-13 14:00:31 +0500

    fix

--- config/config.js
+++ config/config.js
@@ -16,7 +16,6 @@ module.exports = {
     dialect: process.env.DIALECT,
   },
   production: {
-    use_env_variable: process.env.POSTGRES_URL,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
