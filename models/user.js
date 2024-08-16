d11c49e8c0189422ebf07025e6f5425dc77d569a
maham-a-awais <mahamawais888@outlook.com>
2024-08-16 18:02:03 +0500

    removed avatar from user model

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
       email: DataTypes.STRING,
       password: DataTypes.STRING,
       isVerified: DataTypes.BOOLEAN,
-      avatar: DataTypes.TEXT,
+      // avatar: DataTypes.TEXT,
     },
     {
       sequelize,
