0d6946bdfbdf882772df934272c3ca81377c86de
maham-a-awais <mahamawais888@outlook.com>
2024-08-23 22:50:03 +0500

    cloudinary avatar final 4

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(
   cors({
     origin: "*",
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     maxAge: 3600, // 1 hour
